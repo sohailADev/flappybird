18e63f57b06885db3ce84bb70cf900e340ea7398
sohailadev <sohailaslam0707@gmail.com>
2022-12-22 15:04:02 -0500

    added fish and assets also moving triling as well

---
+++ src/imgs/fish/index.ts
@@ -0,0 +1,38 @@
+const fish1 = require("./fish-1.png");
+const fish2 = require("./fish-2.png");
+const fish3 = require("./fish-3.png");
+const fish4 = require("./fish-4.png");
+const fish5 = require("./fish-5.png");
+const fish6 = require("./fish-6.png");
+const fish7 = require("./fish-7.png");
+const fish8 = require("./fish-8.png");
+const fish9 = require("./fish-9.png");
+const fish10 = require("./fish-10.png");
+const fish11 = require("./fish-11.png");
+const fish12 = require("./fish-12.png");
+const fish13 = require("./fish-13.png");
+const fish14 = require("./fish-14.png");
+const fish15 = require("./fish-15.png");
+const fish16 = require("./fish-16.png");
+const fish17 = require("./fish-17.png");
+const fish18 = require("./fish-18.png");
+export default [
+    fish1,
+    fish2,
+    fish3,
+    fish4,
+    fish5,
+    fish6,
+    fish7,
+    fish8,
+    fish9,
+    fish10,
+    fish11,
+    fish12,
+    fish13,
+    fish14,
+    fish15,
+    fish16,
+    fish17,
+    fish18
+]
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,49 @@
-import * as PIXI from 'pixi.js';
+import * as PIXI  from  'pixi.js';
+var bunyImage = require('./imgs/fish/fish-1.png');
+import fishImages from './imgs/fish';
+const background = require("./imgs/background.png");
+const pipe = require("./imgs/pipe.png");
+
+const app = new PIXI.Application({});
+//@ts-ignore
+document.body.appendChild(app.view);
+//class fish
+
+//backgroung
+let bgBack;
+let bgX =1;
+let bgSpeed =-1;
+
+const frames = [];
+for (let x in fishImages)
+{
+   
+    frames.push(PIXI.Texture.from(fishImages[x]));
+}  
+//const bgTexture = PIXI.Assets.load(background);
+const bgTexture = PIXI.Texture.from(background);
+const pipeTexture = PIXI.Texture.from(pipe);
+let backGround = new PIXI.TilingSprite(bgTexture,window.innerWidth,window.innerHeight);
+let pipeHurdle = new PIXI.TilingSprite(pipeTexture);
+backGround.position.set(0,0);
+
+const player = new PIXI.AnimatedSprite(frames); 
+player.x = app.screen.width / 4;
+player.y = app.screen.height / 2;
+player.anchor.set(0.5);
+player.animationSpeed = 0.4;
+player.play();
+player.width=0.5;
+player.height=0.5;
+app.stage.interactive = true;
+app.stage.hitArea = new PIXI.Rectangle(0,0,1000,1000);
+app.stage.addChild(backGround);
+app.stage.addChild(player);
+// Animate the rotation
+app.ticker.add(() => {
+    //player.x + 0.1;
+    bgX = (bgSpeed +bgX);
+    backGround.tilePosition.x = bgX/2;
+});
+
 
